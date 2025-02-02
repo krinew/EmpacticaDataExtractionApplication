08e6620a98b18f32c89b11088bbae456d6f29223
krinew <147940714+krinew@users.noreply.github.com>
2025-02-02 15:11:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.empatica.empalink.EmpaDeviceManager;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import com.empatica.empalink.ConnectionNotAllowedException;
+import com.empatica.empalink.EmpaDeviceManager;
+import com.empatica.empalink.EmpaticaDevice;
+import com.empatica.empalink.config.EmpaSensorStatus;
+import com.empatica.empalink.config.EmpaSensorType;
+import com.empatica.empalink.config.EmpaStatus;
+import com.empatica.empalink.delegate.EmpaDataDelegate;
+import com.empatica.empalink.delegate.EmpaStatusDelegate;
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements EmpaDataDelegate,EmpaStatusDelegate{
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .setAnchorView(R.id.fab).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void didReceiveGSR(float gsr, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveBVP(float bvp, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveIBI(float ibi, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveTemperature(float t, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveAcceleration(int x, int y, int z, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveBatteryLevel(float level, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveTag(double timestamp) {
+
+    }
+
+    @Override
+    public void didUpdateStatus(EmpaStatus status) {
+
+    }
+
+    @Override
+    public void didEstablishConnection() {
+
+    }
+
+    @Override
+    public void didUpdateSensorStatus(int status, EmpaSensorType type) {
+
+    }
+
+    @Override
+    public void didDiscoverDevice(EmpaticaDevice device, String deviceLabel, int rssi, boolean allowed) {
+
+    }
+
+    @Override
+    public void didFailedScanning(int errorCode) {
+
+    }
+
+    @Override
+    public void didRequestEnableBluetooth() {
+
+    }
+
+    @Override
+    public void bluetoothStateChanged() {
+
+    }
+
+    @Override
+    public void didUpdateOnWristStatus(int status) {
+
+    }
+}
